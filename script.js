afb7b2dce5945d0a07b2923bb3a6e6045bbc3205
ZorionOhhNo <159297424+ZorionOhhNo@users.noreply.github.com>
2025-06-19 13:23:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿function toggleSpoiler() {
+    const spoiler = document.getElementById("spoiler-version");
+    const noSpoiler = document.getElementById("no-spoiler-version");
+    const button = document.querySelector(".toggle-button");
+
+    if (spoiler.style.display === "none") {
+        spoiler.style.display = "block";
+        noSpoiler.style.display = "none";
+        button.textContent = "Parādīt bez spoileri";
+    } else {
+        spoiler.style.display = "none";
+        noSpoiler.style.display = "block";
+        button.textContent = "Parādīt pilnu versiju";
+    }
+}
